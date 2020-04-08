f0bba9aae9c2d850f8037922c3bd66bb5be972e6
Brandon Pollack <brandonpollack23@gmail.com>
2020-04-08 10:48:03 -0700

    Small setup and tested manifest dir

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,10 @@
+use sarekt::renderer::VulkanRenderer;
+use winit::event_loop::EventLoop;
+use winit::window::Window;
+use std::sync::Arc;
+
 fn main() {
-    println!("Hello, world!");
+    let event_loop = EventLoop::new();
+    let window = Arc::new(Window::new(&event_loop).unwrap());
+    let renderer = VulkanRenderer::new(window, 800, 600).unwrap();
 }
